b9872fc3a93e926335976cc0f5cee2d539194fc9
yius <71093504+suillix@users.noreply.github.com>
2021-11-10 21:48:43 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -102,7 +102,7 @@ def handle_text(message):
         bot.send_message(message.chat.id, "у меня тоже могла быть жабья семья...")
         log(message, answer)
     elif message.text == "гд" or message.text == "@all":
-        answer = "@suiiyiux @Belyank1n @makaroshnaya @grustnyichai @RinnaTyan @youohomi"
+        answer = "@suiiyiux @Belyank1n @makaroshnaya @grustnyichai @RinnaTyan @youohomi @desbesh"
         bot.send_message(message.chat.id,"@suiiyiux @Belyank1n @makaroshnaya @grustnyichai @RinnaTyan @youohomi @desbesh")
         log(message, answer)
     elif message.text == "Роберт":
