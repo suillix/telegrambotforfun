45c51a501bce43b2a3c13455045c203be97c1af2
yius <71093504+suillix@users.noreply.github.com>
2021-12-17 11:33:28 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,7 +33,7 @@ def handle_text(message):
         answer = "Покормить жабу"
         log(message, answer)
         bot.send_message(message.chat.id, "Покормить жабу")
-        context.job_queue.run_daily()(callback_minute, interval=10, first=30, context=update.message.chat_id)
+        context.job_queue.run_daily()(callback_minute, interval=0, first=0, context=update.message.chat_id)
     if "Кто солнышко" in message.text:
         answer = constants.random_message2
         bot.send_message(message.chat.id, constants.random_message2())
